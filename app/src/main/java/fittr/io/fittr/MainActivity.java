a3cdc0c282940a25fd16eba5eb2e5a80b648d7f5
Creston <creston@bunch.im>
2015-03-21 02:58:31 -0400

    Added Wolfram Alpha API

--- app/src/main/java/fittr/io/fittr/MainActivity.java
+++ app/src/main/java/fittr/io/fittr/MainActivity.java
@@ -3,20 +3,16 @@ package fittr.io.fittr;
 import java.util.Locale;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -41,6 +37,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // test
+        new SearchTask().execute("pepperoni pizza");
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
---
+++ app/src/main/java/fittr/io/fittr/SearchTask.java
@@ -0,0 +1,118 @@
+package fittr.io.fittr;
+
+import android.os.AsyncTask;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.wolfram.alpha.WAAssumption;
+import com.wolfram.alpha.WAEngine;
+import com.wolfram.alpha.WAException;
+import com.wolfram.alpha.WAPlainText;
+import com.wolfram.alpha.WAPod;
+import com.wolfram.alpha.WAQuery;
+import com.wolfram.alpha.WAQueryResult;
+import com.wolfram.alpha.WASubpod;
+
+/**
+ * Class that queries WA for food nutrition information. Extends the Android Async class. The
+ * output is a list, with the first entry being the number of calories in the food, and the
+ * second entry being a list of search suggestions.
+ *
+ * Created by creston on 3/21/15.
+ */
+public class SearchTask extends AsyncTask<String, Integer, SearchResult> {
+
+    // TODO: remove this from git
+    private static String appid = "G5Q47V-QGJ6JYKHUP";
+    private static final Pattern GRAMS_PATTERN = Pattern.compile("serving size .+ \\(([0-9]+) g\\)");
+    private static final Pattern CALORIES_PATTERN = Pattern.compile("total calories  ([0-9]+)");
+
+    protected SearchResult doInBackground(String... input) {
+
+        // create and perform WA queries with this object
+        WAEngine engine = new WAEngine();
+
+        engine.setAppID(appid);
+        engine.addFormat("plaintext");
+
+        // make query
+        WAQuery query = engine.createQuery();
+        query.setInput(input[0]);
+
+        // default results
+        boolean success = true;
+        Integer grams = 0;
+        Integer calories = 0;
+        List<String> suggestions = new ArrayList<String>();
+
+        try {
+            System.out.println("Query URL:");
+            System.out.println(engine.toURL(query));
+            System.out.println();
+
+            WAQueryResult queryResult = engine.performQuery(query);
+            if (queryResult.isError()) {
+                System.out.println("Query error:");
+                System.out.println("   error code: " + queryResult.getErrorCode());
+                System.out.println("   error message: " + queryResult.getErrorMessage());
+                success = false;
+            } else if (!queryResult.isSuccess()) {
+                System.out.println("Query was not understood; no results available.");
+                success = false;
+            } else {
+                // Got a result
+                System.out.println("Successful query. Pods follow:\n");
+                for (WAPod pod : queryResult.getPods()) {
+                    if (!pod.isError()) {
+                        System.out.println(pod.getTitle());
+                        // find pod with nutrition facts
+                        if (pod.getTitle().toLowerCase().contains("nutrition facts")) {
+                            System.out.println("Found nutrition facts. Continuing.");
+                            for (WASubpod subpod : pod.getSubpods()) {
+                                for (Object element : subpod.getContents()) {
+                                    if (element instanceof WAPlainText) {
+                                        String plaintext = ((WAPlainText) element).getText();
+                                        //System.out.println(plaintext);
+                                        //System.out.println("");
+                                        System.out.println("Extracted the following:");
+                                        Matcher gramMatcher = GRAMS_PATTERN.matcher(plaintext);
+                                        Matcher calsMatcher = CALORIES_PATTERN.matcher(plaintext);
+                                        if (gramMatcher.find()) {
+                                            grams = Integer.parseInt(gramMatcher.group(1));
+                                            System.out.println("Grams: " + grams.toString());
+                                        }
+                                        if (calsMatcher.find()) {
+                                            calories = Integer.parseInt(calsMatcher.group(1));
+                                            System.out.println("Calories: " + calories.toString());
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            System.out.println("Could not find nutrition facts.");
+                            success = false;
+                        }
+
+                        System.out.println("");
+                    }
+                }
+                System.out.println("Printing assumptions");
+                for (WAAssumption assumption : queryResult.getAssumptions()) {
+                    System.out.println("----------");
+                    for (String d : assumption.getDescriptions()) {
+                        System.out.println(d);
+                        suggestions.add(d);
+                    }
+                }
+            }
+        // TODO: warnings, assumptions, other WA stuff
+        } catch (WAException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: return calories
+        return new SearchResult(input[0], grams, calories, suggestions, success);
+    }
+}
