e838b06fbc5cb31ffc57c73e988d99650db32667
Creston <creston@bunch.im>
2015-03-21 19:02:25 -0400

    Use WA to compute calories burned

--- app/src/main/java/fittr/io/fittr/CalorieTask.java
+++ app/src/main/java/fittr/io/fittr/CalorieTask.java
@@ -16,10 +16,22 @@ import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.result.DataReadResult;
+import com.google.android.gms.plus.People;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.wolfram.alpha.WAEngine;
+import com.wolfram.alpha.WAException;
+import com.wolfram.alpha.WAPlainText;
+import com.wolfram.alpha.WAPod;
+import com.wolfram.alpha.WAQuery;
+import com.wolfram.alpha.WAQueryResult;
+import com.wolfram.alpha.WASubpod;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Gets data from Google Fit
@@ -32,8 +44,8 @@ public class CalorieTask extends AsyncTask<Void, Void, Integer> {
     private long startTime;
     private long endTime;
 
-    // TODO: more accurate conversion
-    public static final int CALORIES_PER_STEP = 20;
+    public static final Pattern CALORIES_PATTERN = Pattern.compile("([0-9]+) Cal");
+
 
     private TextView destination;
 
@@ -49,15 +61,29 @@ public class CalorieTask extends AsyncTask<Void, Void, Integer> {
 
         DataReadRequest readRequest = new DataReadRequest.Builder()
                 .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .aggregate(DataType.TYPE_SPEED, DataType.AGGREGATE_SPEED_SUMMARY)
                 .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                 .bucketByTime(1, TimeUnit.DAYS)
+                .setLimit(1)
                 .build();
 
         DataReadResult readResult = Fitness.HistoryApi.readData(
                 mClient, readRequest
         ).await(1, TimeUnit.MINUTES);
 
+        DataReadRequest weightReadRequest = new DataReadRequest.Builder()
+                .read(DataType.TYPE_WEIGHT)
+                .setLimit(1)
+                .setTimeRange(1, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        DataReadResult weightReadResult = Fitness.HistoryApi.readData(
+                mClient, weightReadRequest
+        ).await(1, TimeUnit.MINUTES);
+
         int steps = 0;
+        float weight = 0; // kg
+
         for (DataPoint dp : readResult.getBuckets().get(0).getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA).getDataPoints()) {
             for(Field field : dp.getDataType().getFields()) {
                 if (field.getName().toLowerCase().equals("steps")) {
@@ -65,13 +91,14 @@ public class CalorieTask extends AsyncTask<Void, Void, Integer> {
                 }
             }
         }
+        // TODO: get accurate speed data
+        DataPoint wp = weightReadResult.getDataSet(DataType.TYPE_WEIGHT).getDataPoints().get(0);
+        weight = wp.getValue(wp.getDataType().getFields().get(0)).asFloat();
 
-       // readRequest = new DataReadRequest.Builder()
-      //          .aggregate(DataType.TYPE_)
-
-        int calories = steps / CALORIES_PER_STEP;
+        int calories = calcCalories(weight, steps);
 
         System.out.println("Counted " + steps + " steps which burns " + calories + " calories.");
+        System.out.println("Average weight " + weight);
 
         return calories;
 
@@ -82,5 +109,63 @@ public class CalorieTask extends AsyncTask<Void, Void, Integer> {
         int currentCalCount = Integer.parseInt(destination.getText().toString());
         currentCalCount -= calsBurned;
         destination.setText(currentCalCount + "");
+    }
+
+    private int calcCalories(float weight, int steps) {
+
+        // create and perform WA queries with this object
+        WAEngine engine = new WAEngine();
+
+        engine.setAppID(MainActivity.WA_APP_ID);
+        engine.addFormat("plaintext");
+
+        // make query
+        WAQuery query = engine.createQuery();
+        query.setInput("calories burned from a " + weight + "kg male walking " + steps + "steps");
+
+        // defaults
+        int calories = 0;
+
+        try {
+            System.out.println("Query URL:");
+            System.out.println(engine.toURL(query));
+            System.out.println();
+
+            WAQueryResult queryResult = engine.performQuery(query);
+            if (queryResult.isError()) {
+                System.out.println("Query error:");
+                System.out.println("   error code: " + queryResult.getErrorCode());
+                System.out.println("   error message: " + queryResult.getErrorMessage());
+            } else if (!queryResult.isSuccess()) {
+                System.out.println("Query was not understood; no results available.");
+            } else {
+                // Got a result
+                System.out.println("Successful query. Pods follow:\n");
+                for (WAPod pod : queryResult.getPods()) {
+                    if (!pod.isError()) {
+                        System.out.println(pod.getTitle());
+                        // find pod with input interpretation
+                        if (pod.getTitle().toLowerCase().contains("metabolic properties")) {
+                            for (WASubpod subpod : pod.getSubpods()) {
+                                for (Object element : subpod.getContents()) {
+                                    if (element instanceof WAPlainText) {
+                                        String plaintext = ((WAPlainText) element).getText();
+                                        Matcher calsMatcher = CALORIES_PATTERN.matcher(plaintext);
+                                        if (calsMatcher.find()) {
+                                            calories = Integer.parseInt(calsMatcher.group(1));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // TODO: warnings, assumptions, other WA stuff
+        } catch (WAException e) {
+            e.printStackTrace();
+        }
+
+        return calories;
     }
 }
